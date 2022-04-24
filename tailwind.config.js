e7e6db1638f788316c5557aa26054a1c889d7fe8
KernelPony <rene.hotho@advocado.com>
2022-04-24 10:46:49 +0200

    init tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
+/* eslint-disable no-undef */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      body: ['"Open Sans"'],
+    },
     extend: {},
   },
   plugins: [],
